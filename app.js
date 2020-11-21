0f56db18ea8ddfaed0402b31bedbdb7b12ab722e
Stergios Anastasiadis <Stelioslonx@outlook.com>
2020-11-21 07:23:08 +0100

    My Newsletter SignUp Page

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const fs = require("fs");
+const dataApi = fs.readFileSync("api.json"); //request the api key and List ID from an external file
+const jsonApi = JSON.parse(dataApi);
+const apiKey = jsonApi[0].api;
+const listId = jsonApi[0].id;
+
+const app = express();
+
+app.use(express.static("public")); //in order to be able to load static files to our server
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [{
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName
+      }
+    }]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us20.api.mailchimp.com/3.0/lists/" + listId;
+
+  const options = {
+    method: "POST",
+    auth: "Stergios:" + apiKey
+  }
+
+  const request = https.request(url, options, function(response) {
+
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    })
+  })
+
+  request.write(jsonData);
+  request.end();
+});
+
+
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+});
+
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000");
+});
