9a97c9fc44cfdca50c03974443049d6003460f78
StergiosAnastasiadis <Stelioslonx@outlook.com>
2021-09-28 20:21:44 +0300

    ES6-Syntax

--- app.js
+++ app.js
@@ -3,19 +3,16 @@ const express = require("express");
 const request = require("request");
 const https = require("https");
 
-const apiKey = env.process.env.API_KEY;
-const listId = env.process.env.LISTED_ID;
-
 const app = express();
 
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", function(req, res) {
+app.post("/", (req, res) => {
   const firstName = req.body.fName;
   const lastName = req.body.lName;
   const email = req.body.email;
@@ -33,22 +30,18 @@ app.post("/", function(req, res) {
 
   const jsonData = JSON.stringify(data);
 
-  const url = "https://us20.api.mailchimp.com/3.0/lists/" + listId;
+  const url = "https://us20.api.mailchimp.com/3.0/lists/" + process.env.ID;
 
   const options = {
     method: "POST",
-    auth: "Stergios:" + apiKey
+    auth: "Stergios:" + process.env.API
   }
 
-  const request = https.request(url, options, function(response) {
+  const request = https.request(url, options, (response) => {
 
-    if (response.statusCode === 200) {
-      res.sendFile(__dirname + "/success.html");
-    } else {
-      res.sendFile(__dirname + "/failure.html");
-    }
+    response.statusCode === 200 ? res.sendFile(__dirname + "/success.html") : res.sendFile(__dirname + "/failure.html");
 
-    response.on("data", function(data) {
+    response.on("data", (data) => {
       console.log(JSON.parse(data));
     })
   })
@@ -58,10 +51,10 @@ app.post("/", function(req, res) {
 });
 
 
-app.post("/failure", function(req, res) {
+app.post("/failure", (req, res) => {
   res.redirect("/");
 });
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port 3000");
 });
